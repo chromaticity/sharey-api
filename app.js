a074d926e89a5f59e1acc0caefa1d933637053dd
Brandon Antillon <bantillon@protonmail.ch>
2020-04-19 15:33:38 -0700

    Added mongoose and mongodb drivers, and config file for credentials.

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+
+// Package imports go here.
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
+import credentials from './configs/credentials.js'
+
+// Initialize express, and the port node listens on
 let app = express();
 let port = 3000
+
+// Initialize mongoose/mongo
+let mongoDbUrl = credentials.mongo_url + credentials.database_name;
+mongoose.connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB. Reason:'));
 
 // Add any necessary routes here.
 import home from './routes/Home/home.js';
---
+++ configs/credentials.js
@@ -0,0 +1,6 @@
+let credentials = {
+    'mongo_url': 'mongodb://127.0.0.1/',
+    'database_name': 'sharey-app'
+};
+
+export default credentials;
